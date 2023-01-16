651bfdd874333728f709cd3ecceb3a8f3a9a9887
Hassan <muhammadhassanchannel786@gmail.com>
2023-01-16 19:41:13 +0500

    deployment test

--- controllers/post.js
+++ controllers/post.js
@@ -6,7 +6,7 @@ export const getPosts = (req, res) => {
 
     db.query(q, [req.query.cat], (err, data) => {
         if (err) return res.status(500).json(err)
-
+        console.log(data)
         return res.status(200).json(data)
     })
 }
